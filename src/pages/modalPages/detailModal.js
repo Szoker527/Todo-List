0064de08ced9484e424516066c1336c452528000
Szoker527 <szoker527@gmail.com>
2023-04-19 14:28:51 +0200

    Added style to detail modal

--- src/pages/modalPages/detailModal.js
+++ src/pages/modalPages/detailModal.js
@@ -14,14 +14,23 @@ function detailMod(obj) {
     myModal.classList.add("modal")
     modalContent.classList.add("modal-detail","modal-content", "main")
     span.classList.add("close", "close-detail")
+    detailTitle.classList.add("detail-title")
+    detailProject.classList.add("detail-project", "detail-content")
+    detailPriority.classList.add("detail-priority", "detail-content")
+    detailDueDate.classList.add("detail-date", "detail-content")
+    detailDescription.classList.add("detail-description", "detail-content")
+
 
     myModal.style.display = "flex"
     span.textContent = "close" 
     detailTitle.textContent = obj.title
-    detailProject.textContent = obj.project 
-    detailPriority.textContent = obj.priority 
-    detailDueDate.textContent = obj.date 
-    detailDescription.textContent = obj.description
+    detailProject.textContent = "Project: " + obj.project 
+    detailPriority.textContent = "Priority: " + obj.priority 
+    detailDueDate.textContent = "Due Date: " + obj.date 
+    detailDescription.textContent = "Description: " + obj.description
+
+    const detailContent = document.querySelectorAll(".detail-content")
+    console.log(detailContent)
 
     span.addEventListener("click", () => {
         myModal.style.display = "none"
