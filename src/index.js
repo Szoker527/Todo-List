b9f774df734023788e973c2be31b979241be70c7
Szoker527 <szoker527@gmail.com>
2023-04-21 17:27:51 +0200

    Reworked site to be on html started with header and menu

--- src/index.js
+++ src/index.js
@@ -1,84 +1,84 @@
-import style from "./styles/style.css"
-import { firstLoad, firstLoadModal } from "./functions/firstLoad"
-import { createTask } from "./functions/tasks"
-import { dateArray } from "./functions/arrays"
+import style from "./styles/styles.css"
+// import { firstLoad, firstLoadModal } from "./functions/firstLoad"
+// import { createTask } from "./functions/tasks"
+// import { dateArray } from "./functions/arrays"
 
-// createTask("2014-02-03", "What to do", "wdadadadadaada", "low")
-// createTask("2014-01-04", "Play games", "xxxxxxxxxxxxxxxxx", "medium")
-// createTask("2014-03-05", "Report about dolphins", "xxxxxxxxxxxxxxxxx", "high")
-// createTask("2023-04-14", "Talk to Jarret", "xxxxxxxxxxxxxxxxx", "high")
-// createTask("2023-04-16", "Bring Boss coffe", "gggggg", "low")
-// createTask("2023-04-15", "Delete xxx files", "gggggg", "high")
+// // createTask("2014-02-03", "What to do", "wdadadadadaada", "low")
+// // createTask("2014-01-04", "Play games", "xxxxxxxxxxxxxxxxx", "medium")
+// // createTask("2014-03-05", "Report about dolphins", "xxxxxxxxxxxxxxxxx", "high")
+// // createTask("2023-04-14", "Talk to Jarret", "xxxxxxxxxxxxxxxxx", "high")
+// // createTask("2023-04-16", "Bring Boss coffe", "gggggg", "low")
+// // createTask("2023-04-15", "Delete xxx files", "gggggg", "high")
 
-firstLoad()
+// firstLoad()
 
-function updateStorage() {
-    localStorage.setItem("tasks", JSON.stringify(dateArray));
-}
+// function updateStorage() {
+//     localStorage.setItem("tasks", JSON.stringify(dateArray));
+// }
   
-function restoreDefault() {
-    const defaultArray = [
-        {
-            project: "work",
-            title: "What to do",
-            description: "wdadadadadaada",
-            priority: "low",
-            date: "2014-02-03",
-            id: 0
-        },
-        {
-            project: "work",
-            title: "What to do",
-            description: "wdadadadadaada",
-            priority: "low",
-            date: "2014-02-03",
-            id: 1
-        },
-        {
-            project: "relax",
-            title: "Play games",
-            description: "xxxxxxxxxxxxxxxxx",
-            priority: "medium",
-            date: "2014-01-04",
-            id: 2
-        },
-        {
-            project: "work",
-            title: "Report about dolphins",
-            description: "xxxxxxxxxxxxxxxxx",
-            priority: "high",
-            date: "2014-03-05",
-            id: 3
-        },
-        {
-            project: "work",
-            title: "Talk to Jarret",
-            description: "xxxxxxxxxxxxxxxxx",
-            priority: "high",
-            date: "2023-04-14",
-            id: 4
-        },
-        {
-            project: "work",
-            title: "Bring Boss coffe",
-            description: "gggggg",
-            priority: "low",
-            date: "2023-04-16",
-            id: 5
-        },
-        {
-            project: "relax",
-            title: "Delete xxx files",
-            description: "gggggg",
-            priority: "high",
-            date: "2023-04-15",
-            id: 6
-        }
-    ]
-    localStorage.setItem("tasks", JSON.stringify(defaultArray));
-}
+// function restoreDefault() {
+//     const defaultArray = [
+//         {
+//             project: "work",
+//             title: "What to do",
+//             description: "wdadadadadaada",
+//             priority: "low",
+//             date: "2014-02-03",
+//             id: 0
+//         },
+//         {
+//             project: "work",
+//             title: "What to do",
+//             description: "wdadadadadaada",
+//             priority: "low",
+//             date: "2014-02-03",
+//             id: 1
+//         },
+//         {
+//             project: "relax",
+//             title: "Play games",
+//             description: "xxxxxxxxxxxxxxxxx",
+//             priority: "medium",
+//             date: "2014-01-04",
+//             id: 2
+//         },
+//         {
+//             project: "work",
+//             title: "Report about dolphins",
+//             description: "xxxxxxxxxxxxxxxxx",
+//             priority: "high",
+//             date: "2014-03-05",
+//             id: 3
+//         },
+//         {
+//             project: "work",
+//             title: "Talk to Jarret",
+//             description: "xxxxxxxxxxxxxxxxx",
+//             priority: "high",
+//             date: "2023-04-14",
+//             id: 4
+//         },
+//         {
+//             project: "work",
+//             title: "Bring Boss coffe",
+//             description: "gggggg",
+//             priority: "low",
+//             date: "2023-04-16",
+//             id: 5
+//         },
+//         {
+//             project: "relax",
+//             title: "Delete xxx files",
+//             description: "gggggg",
+//             priority: "high",
+//             date: "2023-04-15",
+//             id: 6
+//         }
+//     ]
+//     localStorage.setItem("tasks", JSON.stringify(defaultArray));
+// }
 
-  console.log(dateArray)
+//   console.log(dateArray)
   
 
-export {updateStorage, restoreDefault}
+// export {updateStorage, restoreDefault}
